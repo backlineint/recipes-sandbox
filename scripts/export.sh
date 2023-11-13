ca0e410597b02ffd3f855b61be8d62d91b22dbc1
Brian Perry <contact.brianperry@gmail.com>
2023-11-13 15:25:57 -0600

    Add presentation content type

--- scripts/export.sh
+++ scripts/export.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-lando drush cex
+lando drush cex -y
 rm -rf web/recipes/custom/slidez/config
 mkdir web/recipes/custom/slidez/config
 cp ./config/sync/*slidez* ./web/recipes/custom/slidez/config/
