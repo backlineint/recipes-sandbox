2cd543d6abe215c6ae12992caee521a3b85388d5
Brian Perry <contact.brianperry@gmail.com>
2023-11-14 09:22:25 -0600

    Generate dummy content during init

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,3 +5,6 @@ lando start
 lando drush si -y
 lando drush upwd admin admin
 lando ssh -c "cd web && php core/scripts/drupal recipe recipes/custom/slidez"
+lando drush en devel_generate -y
+lando drush genc 10 --bundles=slidez_slide
+lando drush genc 1 --bundles=slidez_presentation
