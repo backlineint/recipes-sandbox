4cb79af988073704923ecc6fe4f59bcb61d14e72
Brian Perry <contact.brianperry@gmail.com>
2023-11-15 09:55:27 -0600

    Remove title from view

--- scripts/export.sh
+++ scripts/export.sh
@@ -4,3 +4,5 @@ lando drush cex -y
 rm -rf web/recipes/custom/slidez/config
 mkdir web/recipes/custom/slidez/config
 cp ./config/sync/*slidez* ./web/recipes/custom/slidez/config/
+find web/recipes/custom/slidez/config -type f -exec sed -i '' '/^uuid: /d' {} \;
+find web/recipes/custom/slidez/config -type f -exec sed -i '' '/_core:/{N;d;}' {} \;
